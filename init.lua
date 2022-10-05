2c70ad5c873064d8cd8e2fd9df77d7f3b75fcf49
David von Tamar <von.tamar@outlook.com>
2022-10-05 08:01:19 +0300

    Defining default colors and styles.

--- init.lua
+++ init.lua
@@ -10,16 +10,34 @@ local analyzers = {
 local indicators = {}
 --------------------------------------------------------------------------------
 local function init_styles()
+	-- Default Colors
+	-- These colors are used if the current theme does not define them.
+	if not view.colors["error"] then view.colors["error"] = 0x3c14dc end
+	if not view.colors["warning"] then view.colors["warning"] = 0x00c0ff end
+	-- Default Styles
+	-- These styles are used if the current theme does not define them.
+	if not view.styles["error"] then
+		view.styles["error"] = {
+			fore = 0xffffff,
+			back = view.colors["error"],
+		}
+	end
+	if not view.styles["warning"] then
+		view.styles["warning"] = {
+			fore = 0x000000,
+			back = view.colors["error"],
+		}
+	end
 	-- Error
 	indicators.error = _SCINTILLA.next_indic_number()
 	view.indic_style[indicators.error] = view.INDIC_SQUIGGLEPIXMAP
-	view.indic_fore[indicators.error] = 0x3C14DC
+	view.indic_fore[indicators.error] = view.colors["error"]
 	-- Warning
 	indicators.warning = _SCINTILLA.next_indic_number()
 	view.indic_style[indicators.warning] = view.INDIC_SQUIGGLEPIXMAP
-	view.indic_fore[indicators.warning] = 0x00C0FF
+	view.indic_fore[indicators.warning] = view.colors["warning"]
 	-- Annotation type
-	--view.eol_annotation_visible = view.EOLANNOTATION_STANDARD
+	view.eol_annotation_visible = view.EOLANNOTATION_STANDARD
 end
 --------------------------------------------------------------------------------
 local function get_command(analyzer)
@@ -55,7 +73,7 @@ local function analyze_file()
 	buffer:indicator_clear_range(0, buffer.length)
 	buffer.indicator_current = indicators.warning
 	buffer:indicator_clear_range(0, buffer.length)
-	buffer:eol_annotation_clear_all()
+	buffer:annotation_clear_all()
 	-- Mark the errors.
 	local error_index = 0
 	buffer.indicator_current = indicators.error
@@ -64,6 +82,7 @@ local function analyze_file()
 		buffer:indicator_fill_range(at, issue.length)
 		buffer.eol_annotation_text[issue.line] =
 			buffer.eol_annotation_text[issue.line]..issue.message.."; "
+		buffer.eol_annotation_style[issue.line] = buffer:style_of_name("error")
 	end
 	-- Mark the warnings.
 	local warning_index = 0
@@ -73,6 +92,7 @@ local function analyze_file()
 		buffer:indicator_fill_range(at, issue.length)
 		buffer.eol_annotation_text[issue.line] =
 			buffer.eol_annotation_text[issue.line]..issue.message.."; "
+		buffer.eol_annotation_style[issue.line] = buffer:style_of_name("warning")
 	end
 	-- Notify the user.
 	local summary = ""
@@ -94,8 +114,6 @@ end
 --------------------------------------------------------------------------------
 events.connect(events.INITIALIZED, init_styles)
 events.connect(events.FILE_AFTER_SAVE, analyze_file)
-events.connect(events.BUFFER_AFTER_SWITCH, analyze_file)
-events.connect(events.BUFFER_NEW, analyze_file)
 --------------------------------------------------------------------------------
 -- EOF
 --------------------------------------------------------------------------------
